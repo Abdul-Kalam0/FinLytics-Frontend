8c37b0e2780d1ad460485bef9c0df827f18e0546
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-05-01 11:11:19 +0530

    Initialized redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counterSlice";
+import { useState } from "react";
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1>Hii</h1>
+      <div>
+        <button>Balance</button>
+        <button>Income</button>
+        <button>Expenses</button>
+      </div>
+      // Transaction form
+      <div>
+        <h1>Transaction Form</h1>
+      </div>
+      //// Transaction history
+      <div>
+        <h1>Transaction History</h1>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <input
+          type="number"
+          value={num}
+          onChange={(e) => setNum(e.target.value)}
+        />
+        <button onClick={() => dispatch(incrementByAmount(Number(num)))}>
+          Increment by amount
+        </button>
+      </div>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import { Register } from "./pages/Register.jsx";
 import { Login } from "./pages/Login.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { Profile } from "./pages/Profile.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 const router = createBrowserRouter([
   {
@@ -37,17 +39,19 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnHover
-        draggable
-      />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          draggable
+        />
+      </AuthProvider>
+    </Provider>
   </>,
 );
